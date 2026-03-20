9c88f673aa30b22480e61c1ef98e87c37b3ae9c9
Patrickboules <patrickblb05@gmail.com>
2026-03-20 21:03:22 +0200

    refactor: make maintenance request class to be non abstract

--- src/BusinessLayer/Domain/UrgentMaintenanceRequest.java
+++ src/BusinessLayer/Domain/UrgentMaintenanceRequest.java
@@ -4,12 +4,12 @@ import java.time.LocalDateTime;
 
 public class UrgentMaintenanceRequest extends MaintenanceRequest {
 
-    public UrgentMaintenanceRequest(String unitID, String issueDescription) {
-        super(unitID, issueDescription);
+    public UrgentMaintenanceRequest(String unitID, String issueDescription, RequestType requestType) {
+        super(unitID, issueDescription, requestType);
     }
 
     public UrgentMaintenanceRequest(String requestID, String unitID, String issueDescription,
-            LocalDateTime requestDate, String status) {
-        super(requestID, unitID, issueDescription, requestDate, status);
+            LocalDateTime requestDate, String status, RequestType requestType) {
+        super(requestID, unitID, issueDescription, requestDate, status, requestType);
     }
 }
